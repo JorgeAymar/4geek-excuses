ef3fca84930bd96cd9a046fb48d5e187186b8c6e
Jorge Sánchez Aymar <jorge.s.aymar@gmail.com>
2023-01-20 01:01:29 +0000

    ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//write your code here
+// Using javascript, create a function that generates and returns a random excuse with the following structure:
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["El perro", "Mi abuela", "Mi tortuga", "Mi canario", "Mi gato"];
+let what = ["se comió", "aplastó", "robó", "rompió", "quebró"];
+let when = [
+  "antes de clases",
+  "justo cuando terminé",
+  "mientras almorzaba",
+  "mientras yo rezaba"
+];
+
+function Generator(quien, que, cuando) {
+  numQuien = Math.floor(Math.random() * quien.length);
+  numQue = Math.floor(Math.random() * que.length);
+  numCuando = Math.floor(Math.random() * cuando.length);
+  return (
+    quien[numQuien] +
+    " " +
+    que[numQue] +
+    " " +
+    "mi tarea" +
+    " " +
+    cuando[numCuando]
+  );
+}
+
+function Ejecutar() {
+  const excusador = document.getElementById("excusa");
+  let mensaje = Generator(who, what, when);
+  /*console.log(mensaje); */
+  excusador.innerHTML = "<h1>" + mensaje + "</h1>";
+}
+
+window.onload = Ejecutar();
